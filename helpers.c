c18d43f18fb0e62135cd0a2a1c6fdc9a7977ec04
tannyitian <tannyitian@users.noreply.github.com>
2020-08-16 03:40:04 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -118,9 +118,9 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                 }
 
             }
-            image[i][j].rgbtBlue = round(sumblue/count);
-            image[i][j].rgbtRed = round(sumred/count);
-            image[i][j].rgbtGreen = round(sumgreen/count);
+            temp[i][j].rgbtBlue = round(sumblue/count);
+            temp[i][j].rgbtRed = round(sumred/count);
+            temp[i][j].rgbtGreen = round(sumgreen/count);
         }
 
     }
