4df2fb647c2afdeff5f267bcfb3964f4c111550a
tannyitian <tannyitian@users.noreply.github.com>
2020-08-16 04:58:22 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -135,10 +135,6 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-    // int sBlue;
-    // int sRed;
-    // int sGreen;
-    // float count;
     RGBTRIPLE temp[height][width];
     for (int i = 0; i < height; i++)
     {
