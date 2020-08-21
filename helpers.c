6a105f248385c44446edfa90080d336b403a7a26
tannyitian <tannyitian@users.noreply.github.com>
2020-08-21 16:12:48 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -27,14 +27,17 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             float count = 0.00;
             for (int k = -1; k < 2; k++)
             {
-                if (i + k < 0 || i + k > height - 1 || j + k < 0 || j + k > width - 1 )
+                for (int y = -1; y<2; y++)
                 {
-                    continue;
+                    if (i + k < 0 || i + k > height - 1 || j + y < 0 || j + y > width - 1 )
+                    {
+                        continue;
+                    }
+                    sBlue += image[i + k][j + k].rgbtBlue;
+                    sRed += image[i + k][j + k].rgbtRed;
+                    sGreen += image[i + k][j + k].rgbtGreen;
+                    count++;
                 }
-                sBlue += image[i + k][j + k].rgbtBlue;
-                sRed += image[i + k][j + k].rgbtRed;
-                sGreen += image[i + k][j + k].rgbtGreen;
-                count++;
             }
             temp[i][j].rgbtBlue = round(sBlue / count);
             temp[i][j].rgbtRed = round(sRed / count);
@@ -55,115 +58,116 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 }
 
 
-
-//Detect edges
+// Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
-    // RGBTRIPLE temp[height][width];
-    // int gx[3][3];
-    // int gy[3][3];
+//     RGBTRIPLE temp[height][width];
+//     int gx[3][3];
+//     int gy[3][3];
 
-    // for (int i = 0; i < 3; i++)
-    // {
-    //     for(int j = 0; j < 3; j++)
-    //     {
-    //         if (j == 0)
-    //         {
-    //             if(i==0 || i==2)
-    //                 gx[i][j] = -1;
+//     for (int i = 0; i < 3; i++)
+//     {
+//         for(int j = 0; j < 3; j++)
+//         {
+//             if (j == 0)
+//             {
+//                 if(i==0 || i==2)
+//                     gx[i][j] = -1;
 
-    //             else
-    //                 gx[i][j] = -2;
+//                 else
+//                     gx[i][j] = -2;
 
-    //         }
-    //         else if (j == 2)
-    //         {
-    //             if(i==0 || i==2)
-    //                 gx[i][j] = 1;
+//             }
+//             else if (j == 2)
+//             {
+//                 if(i==0 || i==2)
+//                     gx[i][j] = 1;
 
-    //             else
-    //                 gx[i][j] = 2;
-    //         }
-    //         else
-    //         {
-    //             gx[i][j] = 0;
-    //         }
+//                 else
+//                     gx[i][j] = 2;
+//             }
+//             else
+//             {
+//                 gx[i][j] = 0;
+//             }
 
-    //     }
-    // }
+//         }
+//     }
 
-    // for (int i = 0; i < 3; i++)
-    // {
-    //     for(int j = 0; j < 3; j++)
-    //     {
-    //         if (i == 0)
-    //         {
-    //             if(j==0 || j==2)
-    //                 gy[i][j] = -1;
+//     for (int i = 0; i < 3; i++)
+//     {
+//         for(int j = 0; j < 3; j++)
+//         {
+//             if (i == 0)
+//             {
+//                 if(j==0 || j==2)
+//                     gy[i][j] = -1;
 
-    //             else
-    //                 gy[i][j] = -2;
+//                 else
+//                     gy[i][j] = -2;
 
-    //         }
-    //         else if (i == 2)
-    //         {
-    //             if(j==0 || j==2)
-    //                 gy[i][j] = 1;
+//             }
+//             else if (i == 2)
+//             {
+//                 if(j==0 || j==2)
+//                     gy[i][j] = 1;
 
-    //             else
-    //                 gy[i][j] = 2;
-    //         }
-    //         else
-    //         {
-    //             gy[i][j] = 0;
-    //         }
+//                 else
+//                     gy[i][j] = 2;
+//             }
+//             else
+//             {
+//                 gy[i][j] = 0;
+//             }
 
-    //     }
-    // }
+//         }
+//     }
 
-    // for (int i = 0; i < height; i++)
-    // {
-    //     for (int j = 0; j < width; j++)
-    //     {
-    //         int sxBlue, syBlue = 0;
-    //         int sxRed, syBlue = 0;
-    //         int sxGreen, syBlue = 0;
-    //         float count = 0.00;
-    //         for (int k = -1; k < 2; k++)
-    //         {
-    //             if (i + k < 0 || i + k > height - 1)
-    //             {
-    //                 continue;
-    //             }
-    //             for (int y = -1; y < 2; y++)
-    //             {
-    //                 if (j + y < 0 || j + y > width - 1)
-    //                 {
-    //                     continue;
-    //                 }
-    //                 sBlue += image[i + k][j + y].rgbtBlue * gx[i + k][j + y];
-    //                 sRed += image[i + k][j + y].rgbtRed * gx[i + k][j + y];
-    //                 sGreen += image[i + k][j + y].rgbtGreen *gx[i + k][j + y];
+//     for (int i = 0; i < height; i++)
+//     {
+//         for (int j = 0; j < width; j++)
+//         {
+//             int sxBlue, syBlue = 0;
+//             int sxRed, syBlue = 0;
+//             int sxGreen, syBlue = 0;
+//             float count = 0.00;
+//             for (int k = -1; k < 2; k++)
+//             {
+//                 if (i + k < 0 || i + k > height - 1)
+//                 {
+//                     continue;
+//                 }
+//                 for (int y = -1; y < 2; y++)
+//                 {
+//                     if (j + y < 0 || j + y > width - 1)
+//                     {
+//                         continue;
+//                     }
+//                     sBlue += image[i + k][j + y].rgbtBlue * gx[i + k][j + y];
+//                     sRed += image[i + k][j + y].rgbtRed * gx[i + k][j + y];
+//                     sGreen += image[i + k][j + y].rgbtGreen *gx[i + k][j + y];
 
-    //                 count++;
-    //             }
+//                     count++;
+//                 }
 
-    //         }
-    //         temp[i][j].rgbtBlue = round(sBlue / count);
-    //         temp[i][j].rgbtRed = round(sRed / count);
-    //         temp[i][j].rgbtGreen = round(sGreen / count);
-    //     }
-    // }
+//             }
+//             temp[i][j].rgbtBlue = round(sBlue / count);
+//             temp[i][j].rgbtRed = round(sRed / count);
+//             temp[i][j].rgbtGreen = round(sGreen / count);
+//         }
+//     }
 
-    // for (int i = 0; i < height; i++)
-    // {
-    //     for (int j = 0; j < width; j++)
-    //     {
-    //         image[i][j].rgbtBlue = temp[i][j].rgbtBlue;
-    //         image[i][j].rgbtGreen = temp[i][j].rgbtGreen;
-    //         image[i][j].rgbtRed = temp[i][j].rgbtRed;
-    //     }
-    // }
+//     for (int i = 0; i < height; i++)
+//     {
+//         for (int j = 0; j < width; j++)
+//         {
+//             image[i][j].rgbtBlue = temp[i][j].rgbtBlue;
+//             image[i][j].rgbtGreen = temp[i][j].rgbtGreen;
+//             image[i][j].rgbtRed = temp[i][j].rgbtRed;
+//         }
+//     }
 
+// }
     return;
 }
+
