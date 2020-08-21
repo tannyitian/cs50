7c95dcb1cfb0b31c4fdc5c9797d13b214db3a80a
tannyitian <tannyitian@users.noreply.github.com>
2020-08-21 16:14:24 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -4,12 +4,34 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    //make sure that red, green and blue all have the same value
+    //then take the average
+    for (int i=0; i<height; i++)
+    {
+        for (int j=0; j<width; j++)
+        {
+            int avg = round(((float)image[i][j].rgbtBlue + (float)image[i][j].rgbtGreen + (float)image[i][j].rgbtRed) / 3);
+            image[i][j].rgbtRed = avg;
+            image[i][j].rgbtGreen = avg;
+            image[i][j].rgbtBlue = avg;
+        }
+    }
     return;
 }
+
 
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    for(int i=0; i<height;i++)
+    {
+        for(int j=0;j<width/2;j++)
+        {
+            RGBTRIPLE temp = image[i][j];
+            image[i][j] = image[i][width - (j + 1)];
+            image[i][width - (j + 1)] = temp;
+        }
+    }
     return;
 }
 
