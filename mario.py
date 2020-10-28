50159161fdd239662f0cb66b6caf24251e2dd659
tannyitian <tannyitian@users.noreply.github.com>
2020-10-28 06:21:54 +0000

    automated commit by submit50

--- mario.py
+++ mario.py
@@ -1,7 +1,10 @@
 while True:
-    n = int(input("Positive Integer: "))
-    if n>0 and n<8:
-        break
+    try:
+        n = int(input("Height: "))
+        if n>0 and n<=8:
+            break
+    except:
+        continue
 for i in range(n):
     for j in range(n):
         if j < (n-i-1):
